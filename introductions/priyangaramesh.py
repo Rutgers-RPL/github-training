acdc315f94481cb490dfbd1750e860f7f8f86374
Priyanga Ramesh <pr608@scarletmail.rutgers.edu>
2025-10-15 16:55:45 -0400

    Github Training: Priyanga Ramesh

---
+++ introductions/priyangaramesh.py
@@ -0,0 +1,4 @@
+def intro():
+    print('Hello! My name is Priyanga Ramesh and I am a sophomore majoring in Aerospace Engineering!' +
+     'I am minoring in Comp Sci and Astronomy, and I like to draw, listen to music, and play video games.' + 
+     'I have been in RRPL since I was a freshman and hope to continue until I graduate!')
