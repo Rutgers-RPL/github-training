2200219dd09b02df15292123dbdab13d660540e4
Joseph Chavarria <joseph.chavarria@rutgers.edu>
2025-10-15 21:38:07 -0400

    Github Training: Joseph Chavarria

---
+++ introductions/josephchavarria.py
@@ -0,0 +1,8 @@
+def intro():
+    print('Hi! My name is Joseph Chavarria' +
+    '\nI am a CS major, junior, and this is my first year being apart of RPL.' +
+    '\nI\'ll be apart of the software Avionics team!'
+    '\nI\'m apart of the Rutgers Marching Band playing Tenor Sax, and I do game dev\'ing too' +
+    '\nI plan to complete a masters degree specializing in ML/AI.\n'
+    )
+
