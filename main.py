161ee709ebba388de87bfd5014fdfa050a030379
Shyam <shyam@Shyams-MacBook-Pro.local>
2025-10-24 16:56:55 -0400

    Github Traing: *shyam* *srikant*

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from introductions import aaronordonez
 from introductions import mokshithanelluri
 from introductions import matthewschaming
 from introductions import sophiaabbassi
+from introduction import shyamsrikant
 # Put all imports above this line
 
 def main():
@@ -22,6 +23,7 @@ def main():
     mokshithanelluri.intro()
     matthewschaming.intro()
     sophiaabbassi.intro()
+    shyamsrikant.intro()
     # Call you intro function above this line
 
 
